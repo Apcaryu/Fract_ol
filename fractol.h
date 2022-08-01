b5509a618776977494ed93bbb8ef2e16b7f9d507
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-08-01 15:35:39 +0200

    put white pixel

--- fractol.h
+++ fractol.h
@@ -6,11 +6,12 @@
 /*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 13:01:13 by apellegr          #+#    #+#             */
-/*   Updated: 2022/08/01 12:37:50 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/08/01 15:22:17 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx-linux/mlx.h"
+#include <math.h>
 #include <X11/keysym.h>
 #include <X11/X.h>
 #include <fcntl.h>
--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
 
 	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1000, 1000, "Hello world");
+	mlx_pixel_put(mlx, mlx_win, 500, 500, 0x00FFFFFF);
+	mlx_loop(mlx);
 }
