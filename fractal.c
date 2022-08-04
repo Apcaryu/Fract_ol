a88c053958cfba1859ffb5409c78222e0dea5af6
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-08-04 17:09:34 +0200

    les couleurs sur ton chemin

--- fractal.c
+++ fractal.c
@@ -82,24 +82,70 @@ void	mandelbrot(void *mlx, void *mlx_win, void *mlx_image, unsigned int iter)
 	mlx_put_image_to_window(mlx, mlx_win, mlx_image, 0, 0);
 }
 
-void	mandelbrot2()
+void	mandelbrot2(t_data *mlx_data, unsigned int iter)
 {
-	int	px;
-	int	py;
+	char		*pos;
+	unsigned int white = 0x00FFFFFF;
+	unsigned int rose = 0x00FF00FF;
 
-	int	size_x;
-	int	size_y;
+	float	min_x = -2.1;
+	float	max_x = 0.6;
+	float	min_y = -1.2;
+	float	max_y = 1.2;
+	unsigned int	zoom = 100; //100 pixel pour une unite
 	
-	size_x = 
-	px = 0;
-	while (px < size_x)
+	int	image_x = 1000;
+	int image_y = 1000;
+	double zoom_x = image_x/(max_x - min_x);
+	double zoom_y = image_y/(max_y - min_y);
+
+	unsigned int	x = 0;
+	unsigned int	y = 0;
+
+	double c_r = x/zoom+min_x;
+	double c_i = y/zoom+min_y;
+	double	z_r = 0;
+	double	z_i = 0;
+	int	i = 0;
+	double	tmp = z_r;
+
+	while (x < image_x)
 	{
-		py = 0;
-		while (py < size_y)
+		y = 0;
+		while (y < image_y)
 		{
-			py++;
+			pos = mlx_data->img.addr + (y * mlx_data->img.line_len + x * (mlx_data->img.bpp/8));
+			
+
+			c_r = x/zoom_x+min_x;
+			c_i = y/zoom_y+min_y;
+			z_r = 0;
+			z_i = 0;
+			i = 0;
+			tmp = z_r;
+
+			z_r = z_r * z_r - z_i * z_i + c_r;
+			z_i = 2 * z_i * tmp + c_i;
+			i = i + 1;
+
+			while (((z_r * z_r + z_i * z_i) < 4) && i < iter)
+			{
+				tmp = z_r;
+				z_r = (z_r * z_r) - (z_i * z_i) + c_r;
+				z_i = 2 * tmp * z_i + c_i;
+				i = i + 1;
+			}
+			if (i == iter)
+			{
+				*(unsigned int *)pos = white;
+			}
+			else
+			{
+				*(unsigned int *)pos = rose;
+			}
+			y++;
 		}
-		px++;	
+		x++;
 	}
-	
+	mlx_put_image_to_window(mlx_data->mlx, mlx_data->mlx_window, mlx_data->img.mlx_img, 0, 0);
 }
--- fractol.h
+++ fractol.h
@@ -6,18 +6,37 @@
 /*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 13:01:13 by apellegr          #+#    #+#             */
-/*   Updated: 2022/08/03 17:54:52 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/08/04 16:07:39 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+# define WIN_X 1000
+# define WIN_Y 1000
 # include "minilibx-linux/mlx.h"
 # include <math.h>
 # include <X11/keysym.h>
 # include <X11/X.h>
 # include <fcntl.h>
 
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*mlx_window;
+	t_img	img;
+}	t_data;
+
 void	mandelbrot(void *mlx, void *mlx_win, void *mlx_image, unsigned int iter);
+void	mandelbrot2(t_data *mlx_data, unsigned int iter);
 
 #endif
