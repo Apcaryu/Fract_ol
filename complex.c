e44b6ad0153d97a01ff17c8c749d83a9f6c24afa
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-08-11 18:32:55 +0200

    add multiply for complex

--- complex.c
+++ complex.c
@@ -8,3 +8,12 @@ t_cmplx add_cmplx(t_cmplx nb_a, t_cmplx nb_b)
 	nb_out.im = nb_a.im + nb_b.im;
 	return(nb_out);
 }
+
+t_cmplx multiply_cmplx(t_cmplx nb_a, t_cmplx nb_b)
+{
+	t_cmplx	nb_out;
+
+	nb_out.rl = nb_a.rl * nb_b.rl - nb_a.im * nb_b.im;
+	nb_out.im = nb_a.rl * nb_b.im + nb_a.im * nb_b.rl;
+	return(nb_out);
+}
