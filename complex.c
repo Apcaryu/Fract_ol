50292d2f777abb43bf46a7c1e60bc624331649e2
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-08-12 12:31:03 +0200

    add substraction in complex.c

--- complex.c
+++ complex.c
@@ -9,6 +9,15 @@ t_cmplx add_cmplx(t_cmplx nb_a, t_cmplx nb_b)
 	return(nb_out);
 }
 
+t_cmplx	substract_cmplx(t_cmplx nb_a, t_cmplx nb_b)
+{
+	t_cmplx nb_out;
+
+	nb_out.rl = nb_a.rl - nb_b.rl;
+	nb_out.im = nb_a.im - nb_b.im;
+	return(nb_out);
+}
+
 t_cmplx multiply_cmplx(t_cmplx nb_a, t_cmplx nb_b)
 {
 	t_cmplx	nb_out;
--- complex.h
+++ complex.h
@@ -6,7 +6,7 @@
 /*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 17:39:27 by apellegr          #+#    #+#             */
-/*   Updated: 2022/08/12 12:12:25 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/08/12 12:29:57 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ typedef struct s_cmplx
 
 
 t_cmplx add_cmplx(t_cmplx nb_a, t_cmplx nb_b);
+t_cmplx	substract_cmplx(t_cmplx nb_a, t_cmplx nb_b);
 t_cmplx multiply_cmplx(t_cmplx nb_a, t_cmplx nb_b);
 t_cmplx power_cmplx(t_cmplx nb, unsigned int power);
 #endif
