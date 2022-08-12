7179ff15c6c379429bb1cbe507767213a3a336c4
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-08-12 12:13:42 +0200

    add prototype in complex.h

--- complex.h
+++ complex.h
@@ -6,7 +6,7 @@
 /*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 17:39:27 by apellegr          #+#    #+#             */
-/*   Updated: 2022/08/11 17:51:15 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/08/12 12:12:25 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,8 @@ typedef struct s_cmplx
 	double	im;
 }	t_cmplx;
 
+
+t_cmplx add_cmplx(t_cmplx nb_a, t_cmplx nb_b);
+t_cmplx multiply_cmplx(t_cmplx nb_a, t_cmplx nb_b);
+t_cmplx power_cmplx(t_cmplx nb, unsigned int power);
 #endif
