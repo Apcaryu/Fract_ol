f6d5ea15a4da28ba5ba26f609c5c0882262ad5cb
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-09-20 18:08:24 +0200

    norm color.h

--- color.h
+++ color.h
@@ -28,7 +28,7 @@ typedef struct s_rgb
 	unsigned char	b;
 }	t_rgb;
 
-typedef struct	s_color_set
+typedef struct s_color_set
 {
 	unsigned int	a;
 	unsigned int	b;
@@ -37,11 +37,11 @@ typedef struct	s_color_set
 	unsigned int	e;
 }	t_color_set;
 
-t_rgb	hsv_to_rgb(t_hsv hsv_color);
-t_hsv	rgb_to_hsv(t_rgb rgb_color);
-int		rgb_to_int(t_rgb rgb_color);
-t_rgb	int_to_rgb(int int_color);
+t_rgb		hsv_to_rgb(t_hsv hsv_color);
+t_hsv		rgb_to_hsv(t_rgb rgb_color);
+int			rgb_to_int(t_rgb rgb_color);
+t_rgb		int_to_rgb(int int_color);
 
-t_color_set	set_init();
+t_color_set	set_init(void);
 
 #endif
