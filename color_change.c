83a3a99291f145e3ed5ad8077d86c46160c87221
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-09-23 12:28:57 +0200

    add hsv_cycle function

--- color_change.c
+++ color_change.c
@@ -76,6 +76,43 @@ int	galaxy(t_data *mlx_data)
 	return(0);
 }
 
+int	hsv_cycle(t_data *mlx_data)
+{
+	unsigned int	x;
+	unsigned int	y;
+	char			*pos;
+	t_rgb			rgb;
+	t_hsv			hsv;
+
+	x = 0;
+	while (x < WIN_X)
+	{
+		y = 0;
+		while (y < WIN_Y)
+		{
+			pos = mlx_data->img.addr + (y * mlx_data->img.line_len + x * (mlx_data->img.bpp/8));
+			rgb = int_to_rgb(*(unsigned int *)pos);
+			hsv = rgb_to_hsv(rgb);
+			if (hsv.h >= 360)
+			{
+				hsv.h = 0;
+			}
+			else
+			{
+				hsv.h = hsv.h + 2;
+				if (hsv.h >= 360)
+					hsv.h = 0;
+			}
+			rgb = hsv_to_rgb(hsv);
+			*(unsigned int *)pos = rgb_to_int(rgb);
+			y++;
+		}
+		x++;
+	}
+	mlx_put_image_to_window(mlx_data->mlx, mlx_data->mlx_window, mlx_data->img.mlx_img, 0, 0);
+	return(0);
+}
+
 int color_change(t_data *m_data) {
 	if (!m_data->img.is_animated)
 	{
@@ -83,5 +120,7 @@ int color_change(t_data *m_data) {
 			flash(m_data);
 		else if (m_data->img.animation_mod == 2)
 			galaxy(m_data);
+		else if (m_data->img.animation_mod == 3)
+			hsv_cycle(m_data);
 	}
 }
