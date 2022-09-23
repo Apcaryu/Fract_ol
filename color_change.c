ea4ca912baa0de247db01569cad6b52ed79d2a8a
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-09-23 12:26:38 +0200

    add galaxy function

--- color_change.c
+++ color_change.c
@@ -45,10 +45,43 @@ int	flash(t_data *mlx_data)
 	return(0);
 }
 
+int	galaxy(t_data *mlx_data)
+{
+	unsigned int	x;
+	unsigned int	y;
+	char			*pos;
+
+	x = 0;
+	while (x < WIN_X)
+	{
+		y = 0;
+		while (y < WIN_Y)
+		{
+			pos = mlx_data->img.addr + (y * mlx_data->img.line_len + x * (mlx_data->img.bpp/8));
+			if (*(unsigned int *)pos == 0xFFFFFF)
+			{
+				*(unsigned int *)pos = rand();
+			}
+			else if (*(unsigned int *)pos <= 0x0)
+			{
+				*(unsigned int *)pos = 0x010101;
+			}
+			else
+				*(unsigned int *)pos = *(unsigned int *)pos - DE_COLOR;
+			y++;
+		}
+		x++;
+	}
+	mlx_put_image_to_window(mlx_data->mlx, mlx_data->mlx_window, mlx_data->img.mlx_img, 0, 0);
+	return(0);
+}
+
 int color_change(t_data *m_data) {
 	if (!m_data->img.is_animated)
 	{
 		if (m_data->img.animation_mod == 1)
 			flash(m_data);
+		else if (m_data->img.animation_mod == 2)
+			galaxy(m_data);
 	}
 }
