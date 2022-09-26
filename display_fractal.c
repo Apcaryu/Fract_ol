3f75e2572986a49b0a81862986417fd6ca8b579b
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-09-26 14:41:09 +0200

    norm display_fractal.c

--- display_fractal.c
+++ display_fractal.c
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_fractal.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 14:40:41 by apellegr          #+#    #+#             */
+/*   Updated: 2022/09/26 14:40:42 by apellegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fractol.h"
 
 void	is_in_fractal(t_data m_data, char *pos)
 {
-	*(unsigned int *)pos = 0xFFFFFF / ((m_data.zc.z.rl * 10000) / (m_data.zc.z.im * 10000));
+	*(unsigned int *)pos = 0xFFFFFF / ((m_data.zc.z.rl * 10000) / \
+	(m_data.zc.z.im * 10000));
 }
 
 void	is_out_fractal(t_data m_data, char *pos, unsigned int i)
 {
-	*(unsigned int *)pos = 0x00000000 + i  * 0x010101 * 2;
+	*(unsigned int *)pos = 0x00000000 + i * 0x010101 * 2;
 }
