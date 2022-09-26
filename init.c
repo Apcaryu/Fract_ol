a9d75eff9b5216a58dfca96a9c9d6cd795bd57b8
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-09-26 14:55:50 +0200

    norm init.c

--- init.c
+++ init.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 14:54:55 by apellegr          #+#    #+#             */
+/*   Updated: 2022/09/26 14:54:56 by apellegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fractol.h"
 
 void	base_init(t_data *m_data)
@@ -5,7 +17,7 @@ void	base_init(t_data *m_data)
 	m_data->mlx = mlx_init();
 	m_data->mlx_window = mlx_new_window(m_data->mlx, WIN_X, WIN_Y, "Fractal");
 	m_data->img.mlx_img = mlx_new_image(m_data->mlx, WIN_X, WIN_Y);
-	m_data->img.addr = mlx_get_data_addr(m_data->img.mlx_img,\
+	m_data->img.addr = mlx_get_data_addr(m_data->img.mlx_img, \
 	&m_data->img.bpp, &m_data->img.line_len, &m_data->img.endian);
 	m_data->img.zoom = 1;
 	m_data->mouse_pos.x_pos = 0;
