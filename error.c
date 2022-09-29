34ba9e8c9c93652608e345aaea209ac4153225d5
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-09-29 15:14:47 +0200

    clean error.c

--- error.c
+++ error.c
@@ -12,23 +12,6 @@
 
 #include "fractol.h"
 
-char	check_args(char *argv[])
-{
-	int		cmp_res;
-	char	mod;
-	int		size_arg;
-
-	size_arg = ft_strlen(argv[1]);
-	cmp_res = ft_strncmp(argv[1], "mandelbrot", size_arg);
-	if (!cmp_res)
-		mod = 1;
-	else if (!ft_strncmp(argv[1], "julia", size_arg))
-		mod = 2;
-	else
-		mod = 0;
-	return (mod);
-}
-
 void	julia_setup(t_data *m_data, int argc, char *argv[])
 {
 	if (argc == 3)
