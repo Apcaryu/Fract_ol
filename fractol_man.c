2926ff119fb6e131c5bbcc6fad0902272f36eb73
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-09-29 17:22:32 +0200

    add burningship in man

--- fractol_man.c
+++ fractol_man.c
@@ -18,12 +18,14 @@ void	fractol_man(void)
 	write(1, "You made a mistake in the execution command please check the "
 		"following points:\n", 80);
 	write(1, "1. After the name of program you have written "
-		"\033[35mmandelbrot\033[37m or \033[35mjulia\033[37m.\n", 88);
+		"\033[35mmandelbrot\033[37m or \033[35mjulia\033[37m or "
+		"\033[35mburningship\033[37m.\n", 113);
 	write(1, "2. After julia you have written a maximum of 2 numbers which are"
 		" part of the reals\n", 84);
 	write(1, "3. All the parameters are separated by a space.\n", 49);
 	write(1, "Examples of a valid command:\n", 30);
 	write(1, "./fractol mandelbrot\n", 22);
+	write(1, "./fractol burningship\n", 23);
 	write(1, "./fractol julia 0.3\n", 21);
 	write(1, "./fractol julia 0.3 0.6\n", 25);
 }
