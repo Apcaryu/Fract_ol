8b4394001367c132e01cb5d85026fa2156022536
Apcaryu <antoinepellegrin@outlook.fr>
2022-11-11 01:57:34 +0100

    make a real tester

--- tester.sh
+++ tester.sh
@@ -1,9 +1,19 @@
-make re && make clean && valgrind ./fractol julia #> output.txt
-code output.txt
-
-#make re
-#./fractol mandelbrot &
-#./fractol julia &
-#./fractol julia 0.3 &
-#./fractol julia 0.3 0.58 &
-#./fractol burningship &
+if [ $# == 1 ]
+then
+	make re && make clean
+	if [ $1 == 0 ]
+	then
+		valgrind ./fractol julia
+	elif [ $1 == 1 ]
+	then
+		./fractol mandelbrot &
+		./fractol julia &
+		./fractol julia 0.3 &
+		./fractol julia 0.3 0.58 &
+		./fractol burningship &
+	else
+		echo 'Wrong args'
+	fi
+else
+	echo 'Missing or to many args'
+fi
